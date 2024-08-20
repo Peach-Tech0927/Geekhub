447e088460b6cbe0354ba08e2b3a09ad0bb7f5c7
YoshiakiAsahara <asa-yossan@outlook.jp>
2024-08-20 09:36:44 +0900

    layoutを消去して、認証成功時に「認証成功」を表示

--- app/compose/UserButton/UserButton.tsx
+++ app/compose/UserButton/UserButton.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { auth } from "@/auth";
 
 import SigninButton from "../SigninButton/SigninButton";
-import layout from "./userButton.module.scss";
 
 export default async function UserButton() {
   const session = await auth();
@@ -16,16 +15,13 @@ export default async function UserButton() {
     return (
       <div>
         <SigninButton />
-        <img className={layout.userAccountImg} src={imgPath} />
       </div>
     );
 
   imgPath = session.user.image as string;
   return (
     <div>
-      <img className={layout.userAccountImg} src={imgPath} />
-      <pre>{JSON.stringify(session, null, 2)}</pre>
-      <br />
+      <p>認証に成功</p>
     </div>
   );
 }
