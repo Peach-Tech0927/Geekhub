9675c44af09dee085bf6d9b3f33d31752c33b00c
YoshiakiAsahara <asa-yossan@outlook.jp>
2024-08-20 10:01:57 +0900

    写真を追加

--- app/compose/UserButton/UserButton.tsx
+++ app/compose/UserButton/UserButton.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { auth } from "@/auth";
 import SigninButton from "../SigninButton/SigninButton";
+
 export default async function UserButton() {
   const session = await auth();
   const defaultImgPath =
     "https://upload.wikimedia.org/wikipedia/commons/2/2e/Microsoft_Account_Logo.svg";
   const imgPath = session?.user?.image || defaultImgPath;
+
   return !session?.user ? (
     <div>
       <SigninButton />
+      <img src={defaultImgPath} />
     </div>
   ) : (
     <div>
       <p>認証に成功</p>
+      <img src={imgPath} />
     </div>
   );
 }
