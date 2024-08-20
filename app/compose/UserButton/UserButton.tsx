cd329634651c5b2faad282f0db159eacc1805829
YoshiakiAsahara <asa-yossan@outlook.jp>
2024-08-20 09:56:36 +0900

    letを消去しif文をsessionがあるかないかで制御する方法に変更

--- app/compose/UserButton/UserButton.tsx
+++ app/compose/UserButton/UserButton.tsx
@@ -3,19 +3,14 @@ import { auth } from "@/auth";
 import SigninButton from "../SigninButton/SigninButton";
 export default async function UserButton() {
   const session = await auth();
-  let imgPath =
+  const defaultImgPath =
     "https://upload.wikimedia.org/wikipedia/commons/2/2e/Microsoft_Account_Logo.svg";
-
-  //sesstionが無ければsigninを表示
-  if (!session?.user)
-    return (
-      <div>
-        <SigninButton />
-      </div>
-    );
-
-  imgPath = session.user.image as string;
-  return (
+  const imgPath = session?.user?.image || defaultImgPath;
+  return !session?.user ? (
+    <div>
+      <SigninButton />
+    </div>
+  ) : (
     <div>
       <p>認証に成功</p>
     </div>
