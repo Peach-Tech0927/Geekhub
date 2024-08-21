dac855d4ea5f6d346a9b870ca97701075b312be2
YoshiakiAsahara <asa-yossan@outlook.jp>
2024-08-21 19:44:04 +0900

    詳細記事取得apiを作成

---
+++ app/api/blog/[id]/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { main } from "../route";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+//ブログ詳細記事取得api
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+    await main();
+    const post = await prisma.post.findFirst({ where: { id } }); //http://localhost:30000
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
