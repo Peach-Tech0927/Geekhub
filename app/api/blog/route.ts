41d64c0d25ce61143dd12bf3ad62625ace17d81c
YoshiakiAsahara <asa-yossan@outlook.jp>
2024-08-21 19:23:10 +0900

    api用にprisma接続関数を作成

---
+++ app/api/blog/route.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function main() {
+  try {
+    await prisma.$connect();
+  } catch (err) {
+    return Error("DB接続に失敗");
+  }
+}
