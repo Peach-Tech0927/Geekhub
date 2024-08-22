bcc2f74b0b6b98128990be9f6014be05c1747f15
YoshiakiAsahara <asa-yossan@outlook.jp>
2024-08-22 21:08:30 +0900

    prismadb反映

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import prismadb from "@/app/lib/prisma";
+
 //ブログ詳細記事取得api
 export const GET = async (req: Request, res: NextResponse) => {
   try {
     const id: number = parseInt(req.url.split("/blog/")[1]);
-    const post = await prisma.post.findFirst({ where: { id } }); //http://localhost:30000
+    const post = await prismadb.post.findFirst({ where: { id } }); //http://localhost:30000
     return NextResponse.json({ message: "Success", post }, { status: 200 });
   } catch (err) {
     return NextResponse.json({ message: "Error", err }, { status: 500 });
@@ -17,7 +17,7 @@ export const PUT = async (req: Request, res: NextResponse) => {
   try {
     const id: number = parseInt(req.url.split("/blog/")[1]);
     const { title, description } = await req.json();
-    const post = await prisma.post.update({
+    const post = await prismadb.post.update({
       data: { title, description },
       where: { id },
     });
@@ -30,7 +30,7 @@ export const PUT = async (req: Request, res: NextResponse) => {
 export const DELETE = async (req: Request, res: NextResponse) => {
   try {
     const id: number = parseInt(req.url.split("/blog/")[1]);
-    const post = await prisma.post.delete({
+    const post = await prismadb.post.delete({
       where: { id },
     });
     return NextResponse.json({ message: "Success", post }, { status: 200 });
