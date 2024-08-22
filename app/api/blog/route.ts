8ff4479f30dcb9153dc9487c8d3e8ef0093b4b25
YoshiakiAsahara <asa-yossan@outlook.jp>
2024-08-22 14:01:08 +0900

    グローバル変数を設定

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -1,20 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
-
-export async function main() {
-  try {
-    await prisma.$connect();
-  } catch (err) {
-    return Error("DB接続に失敗");
-  }
+declare global {
+  var prisma: PrismaClient | undefined;
 }
+
+const prismadb = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+
+export default prismadb;
 
 // ブログの全記事取得
 export const GET = async (req: Request, res: NextResponse) => {
   try {
-    const posts = prisma.post.findMany();
+    const posts = prismadb.post.findMany();
     return NextResponse.json({ message: "Success", posts }, { status: 200 });
   } catch (err) {
     return NextResponse.json({ message: "Error", err }, { status: 500 });
@@ -25,12 +24,9 @@ export const GET = async (req: Request, res: NextResponse) => {
 export const POST = async (req: Request, res: NextResponse) => {
   try {
     const { title, description } = await req.json();
-    await main();
-    const post = await prisma.post.create({ data: { title, description } });
+    const post = await prismadb.post.create({ data: { title, description } });
     return NextResponse.json({ message: "Success", post }, { status: 201 });
   } catch (err) {
     return NextResponse.json({ message: "Error", err }, { status: 500 });
-  } finally {
-    await prisma.$disconnect();
   }
 };
